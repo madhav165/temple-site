02efe5c53e9783a6de46624be8a734c2acb61a67
madhav165 <madhav165@gmail.com>
2023-03-30 14:37:57 +0530

    changed html

--- create_html.py
+++ create_html.py
@@ -1,38 +1,47 @@
 import modin.pandas as pd
 import os
-import textwrap
-from fpdf import FPDF
+# import textwrap
+# from fpdf import FPDF
+import pdfkit
 import re
-import aspose.words as aw
+# import aspose.words as aw
 
-def text_to_pdf(text, filename):
-    a4_width_mm = 210
-    pt_to_mm = 0.35
-    fontsize_pt = 10
-    fontsize_mm = fontsize_pt * pt_to_mm
-    margin_bottom_mm = 10
-    character_width_mm = 7 * pt_to_mm
-    width_text = a4_width_mm / character_width_mm
+# def text_to_pdf(text, filename):
+#     a4_width_mm = 210
+#     pt_to_mm = 0.35
+#     fontsize_pt = 10
+#     fontsize_mm = fontsize_pt * pt_to_mm
+#     margin_bottom_mm = 10
+#     character_width_mm = 7 * pt_to_mm
+#     width_text = a4_width_mm / character_width_mm
 
-    pdf = FPDF(orientation='P', unit='mm', format='A4')
-    pdf.set_auto_page_break(True, margin=margin_bottom_mm)
-    pdf.add_page()
-    pdf.set_font(family='Courier', size=fontsize_pt)
-    splitted = text.split('\n')
+#     pdf = FPDF(orientation='P', unit='mm', format='A4')
+#     pdf.set_auto_page_break(True, margin=margin_bottom_mm)
+#     pdf.add_page()
+#     pdf.set_font(family='Courier', size=fontsize_pt)
+#     splitted = text.split('\n')
 
-    for line in splitted:
-        lines = textwrap.wrap(line, width_text)
+#     for line in splitted:
+#         lines = textwrap.wrap(line, width_text)
 
-        if len(lines) == 0:
-            pdf.ln()
+#         if len(lines) == 0:
+#             pdf.ln()
 
-        for wrap in lines:
-            pdf.cell(0, fontsize_mm, wrap, ln=1)
+#         for wrap in lines:
+#             pdf.cell(0, fontsize_mm, wrap, ln=1)
 
-    pdf.output(filename, 'F')
+#     pdf.output(filename, 'F')
 
 df1 = pd.read_csv('states.csv')
 df2 = pd.read_csv('temple_details.csv')
+
+# configuring pdfkit to point to our installation of wkhtmltopdf  
+config = pdfkit.configuration(wkhtmltopdf = r"C:\\Program Files\\wkhtmltopdf\\bin\\wkhtmltopdf.exe")  
+  
+# converting html file to pdf file  
+pdfkit.from_file('sample.html', 'output.pdf', configuration = config)
+
+
 
 for index,row in df1.iterrows():
     state_name = row['state']
@@ -40,19 +49,24 @@ for index,row in df1.iterrows():
     state_name = state_name.strip().encode('latin-1', 'replace').decode('latin-1')
     print(state_name)
     state_description = row['description'].strip().encode('latin-1', 'replace').decode('latin-1')
+    state_description = re.sub(r'[^\w_. -]', ' ', state_description).replace('  ', ' ')
+    state_description = state_description.strip().encode('latin-1', 'replace').decode('latin-1')
     
     if not os.path.isdir(os.path.join('India', state_name)):
         os.mkdir(os.path.join('India', state_name))
-    with open(os.path.join('India', state_name, state_name.replace(',','_').replace(' ','_')+'.txt'), 'w') as f:
-        f.write(state_name+'\n')
-        f.write(state_description)
-
-    input_filename = os.path.join('India', state_name, state_name.replace(',','_').replace(' ','_')+'.txt')
-    output_filename = os.path.join('India', state_name, state_name.replace(',','_').replace(' ','_')+'.pdf')
-    with open(input_filename) as f:
-        text = f.read()
-    text_to_pdf(text, output_filename)
-    os.remove(input_filename)
+        html = f'''<!DOCTYPE html>
+        <html>
+        <head>
+        <title>{state_name} Temples</title>
+        </head>
+        <body>
+        <h1>{state_name}</h1>
+        <p>{state_description}</p>
+        </body>
+        </html>
+        '''
+    with open(os.path.join('India', state_name, state_name.replace(',','_').replace(' ','_')+'.html'), 'w') as f:
+        f.write(html)
     
     df3 = df2.loc[df2['state']==row['state']]
     for index2, row2 in df3.iterrows():
@@ -63,22 +77,67 @@ for index,row in df1.iterrows():
         temple_details = row2['temple_details']
         temple_details = re.sub(r'[^\w_. -]', ' ', temple_details).replace('  ', ' ')
         temple_details = temple_details.strip().encode('latin-1', 'replace').decode('latin-1')
-        try:
-            with open(os.path.join('India', state_name, temple_name.replace(',','_').replace(' ','_')+'.txt'), 'w') as f:
-                f.write(temple_name+'\n')
-                f.write(temple_details)
-            input_filename = os.path.join('India', state_name, temple_name.replace(',','_').replace(' ','_')+'.txt')
-            output_filename = os.path.join('India', state_name, temple_name.replace(',','_').replace(' ','_')+'.pdf')
-            with open(input_filename) as f:
-                text = f.read()
-            text_to_pdf(text, output_filename)
-            os.remove(input_filename)
-        except:
-            pass
+        html = f'''<!DOCTYPE html>
+        <html>
+        <head>
+        <title>{temple_name} Temples</title>
+        </head>
+        <body>
+        <h1>{temple_name}</h1>
+        <p>{temple_details}</p>
+        </body>
+        </html>
+        '''
+        with open(os.path.join('India', state_name, temple_name.replace(',','_').replace(' ','_')+'.html'), 'w') as f:
+            f.write(html)
 
         
 
-        # load TXT document
-        doc = aw.Document(input_filename)
-        # save TXT as PDF file
-        doc.save(output_filename, aw.SaveFormat.PDF)
+# for index,row in df1.iterrows():
+#     state_name = row['state']
+#     state_name = re.sub(r'[^\w_. -]', ' ', state_name).replace('  ', ' ')
+#     state_name = state_name.strip().encode('latin-1', 'replace').decode('latin-1')
+#     print(state_name)
+#     state_description = row['description'].strip().encode('latin-1', 'replace').decode('latin-1')
+    
+#     if not os.path.isdir(os.path.join('India', state_name)):
+#         os.mkdir(os.path.join('India', state_name))
+#     with open(os.path.join('India', state_name, state_name.replace(',','_').replace(' ','_')+'.txt'), 'w') as f:
+#         f.write(state_name+'\n')
+#         f.write(state_description)
+
+#     input_filename = os.path.join('India', state_name, state_name.replace(',','_').replace(' ','_')+'.txt')
+#     output_filename = os.path.join('India', state_name, state_name.replace(',','_').replace(' ','_')+'.pdf')
+#     with open(input_filename) as f:
+#         text = f.read()
+#     text_to_pdf(text, output_filename)
+#     os.remove(input_filename)
+    
+#     df3 = df2.loc[df2['state']==row['state']]
+#     for index2, row2 in df3.iterrows():
+#         temple_name = row2['temple_name']
+#         temple_name = re.sub(r'[^\w_. -]', ' ', temple_name).replace('  ', ' ')
+#         temple_name = temple_name.strip().encode('latin-1', 'replace').decode('latin-1')
+#         print(temple_name)
+#         temple_details = row2['temple_details']
+#         temple_details = re.sub(r'[^\w_. -]', ' ', temple_details).replace('  ', ' ')
+#         temple_details = temple_details.strip().encode('latin-1', 'replace').decode('latin-1')
+#         try:
+#             with open(os.path.join('India', state_name, temple_name.replace(',','_').replace(' ','_')+'.txt'), 'w') as f:
+#                 f.write(temple_name+'\n')
+#                 f.write(temple_details)
+#             input_filename = os.path.join('India', state_name, temple_name.replace(',','_').replace(' ','_')+'.txt')
+#             output_filename = os.path.join('India', state_name, temple_name.replace(',','_').replace(' ','_')+'.pdf')
+#             with open(input_filename) as f:
+#                 text = f.read()
+#             text_to_pdf(text, output_filename)
+#             os.remove(input_filename)
+#         except:
+#             pass
+
+        
+
+        # # load TXT document
+        # doc = aw.Document(input_filename)
+        # # save TXT as PDF file
+        # doc.save(output_filename, aw.SaveFormat.PDF)
